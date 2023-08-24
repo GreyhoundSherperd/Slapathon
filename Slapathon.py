b8f3df39e7fefc06eb30b7ebf9210dcca87b470c
GreenRing <141900921+GreyhoundSherperd@users.noreply.github.com>
2023-08-24 16:40:42 +0700

    Add files via upload

--- Slapathon.py
+++ Slapathon.py
@@ -1,17 +1,14 @@
+
+
 from random import shuffle
 from random import randint
 
 deck = []
-for v in range(2):
+for v in range(4):
     appenddeck = 1
     while appenddeck <= 10:
         deck.append(appenddeck)
         appenddeck += 1
-
-for v in range(4):
-    deck.append("+2")
-    deck.append("+4")
-    deck.append("WILDCARD")
 
 shuffle(deck)
 
@@ -31,17 +28,9 @@ PlayerOfficer = []
 BotOfficer = []
 PlayerSols = []
 BotSols = []
-Warnings = 0
+warnings = 0
 
 def ordering():
-    """
-    Calculates the order of cards in a player's deck.
-
-    This function takes no parameters.
-
-    Returns:
-        str: A string representing the order of cards in the player's deck. Each card is followed by a space and the number of occurrences of that card in the deck.
-    """
     dict = {}
     for v in PlayerDeck:
         if v not in dict:
@@ -51,7 +40,7 @@ def ordering():
     
     strng = ""
     for x in dict:
-        strng += str(x) + " " + str(dict[x]) + " "
+        strng += "Rank " + str(x) + " " + str(dict[x]) + ","
 
     return strng
 
@@ -60,16 +49,16 @@ print("*** For the rules, please see the README.md file. ***")
 print("***                   Good luck!                  ***")
 print("")
 
-while (BotHP != 0 or PlayerHP != 0) and (lenB != 0 or lenP != 0):
+while (lenB != 0) or (lenP != 0):
     PlayerDeck.sort()
     BotDeck.sort()
-    print("Your cards: ", PlayerDeck)
+    print("Your cards: ", ordering())
     cmd = input("Make a command: ")
     if cmd == "Play Soldier":
         if len(PlayerSols) >= 2:
             print("Sorry, your soldier slots have already been filled!")
             print("Penalty: Lose a turn")
-            Warnings += 1
+            warnings += 1
         else:
             cmd = int(input("Choose your desired soldier: "))
             if cmd in PlayerDeck:
@@ -79,13 +68,13 @@ while (BotHP != 0 or PlayerHP != 0) and (lenB != 0 or lenP != 0):
             else:
                 print("That soilder does not exist!")
                 print("Penalty: Lose a turn")
-                Warnings += 1
+                warnings += 1
 
     elif cmd == "Play Officer":
         if len(PlayerOfficer) >= 1:
             print("Sorry, your officer slot has already been filled!")
             print("Penalty: Lose a turn")
-            Warnings += 1
+            warnings += 1
         else:
             cmd = int(input("Choose your desired officer (5 Strength or Above): "))
             if cmd in PlayerDeck:
@@ -95,18 +84,18 @@ while (BotHP != 0 or PlayerHP != 0) and (lenB != 0 or lenP != 0):
             else:
                 print("That officer does not exist!")
                 print("Penalty: Lose a turn")
-                Warnings += 1
+                warnings += 1
 
     elif cmd == "Soldier Attack":
         if len(PlayerSols) == 0:
             print("You do not have any soldiers!")
             print("Penalty: Lose a turn")
-            Warnings += 1
+            warnings += 1
         else:
             if BotOfficer == [] and BotSols == []:
                 print("There is nothing to attack!")
                 print("Penalty: Lose a turn")
-                Warnings += 1
+                warnings += 1
             else:
                 cmd = int(input("Choose your desired soldier to attack: "))
                 if cmd in PlayerSols:
@@ -120,26 +109,26 @@ while (BotHP != 0 or PlayerHP != 0) and (lenB != 0 or lenP != 0):
                         elif currentAttacker < currentDefender:
                             PlayerSols.remove(currentAttacker)
                             BotSols[BotSols.index(currentDefender)] -= currentAttacker
-                            print("Your soldier has dealt damage on the enemy 💥😨 ")
+                            print("You lost the fight, but your soldier has dealt damage on the enemy 💥😨 ")
                     else:
                         print("That soldier does not exist!")
                         print("Penalty: Lose a turn")
-                        Warnings += 1
+                        warnings += 1
                 else:
                     print("That soldier does not exist!")
                     print("Penalty: Lose a turn")
-                    Warnings += 1
+                    warnings += 1
     
     elif cmd == "Officer Attack":
         if len(PlayerOfficer) == 0:
             print("You do not have any officers!")
             print("Penalty: Lose a turn")
-            Warnings += 1
+            warnings += 1
         else:
             if BotOfficer == [] and BotSols == []:
                 print("There is nothing to attack!")
                 print("Penalty: Lose a turn")
-                Warnings += 1
+                warnings += 1
             else:
                 currentAttacker = BotOfficer[0]
                 cmd = int(input("Choose your desired target 🎯: "))
@@ -170,5 +159,76 @@ while (BotHP != 0 or PlayerHP != 0) and (lenB != 0 or lenP != 0):
                                 else:
                                     pass
                             else:
-                                print("Unfortunately, your officer is injured! 🤕🏥")
-                                print("But, with your help, he healed and decided to retire. 🧓🏼📺")
+                                print("Unfortunately, your officer is injured badly! 🤕🏥")
+                                PlayerOfficer.pop()
+                    else:
+                        if currentDefender in BotSols:
+                            experienceExtra = randint(0, 3)
+                            if currentDefender - (currentAttacker + experienceExtra) <= 0:
+                                BotSols.remove(currentDefender)
+                                PlayerOfficer.pop()
+                                if len(PlayerSols) >= 2:
+                                    print("As people says, matter and anti-matter destroy both each other... ⚗️💥")
+                                else:
+                                    PlayerSols.append(1)
+                                    print("Your officer had an exhausting fight! 🥱💤")
+                                    print("Your officer is demoted to a 1 rank soldier! 💂")
+                            else:
+                                PlayerOfficer.pop()
+                                print("Your officer had been defeated! 😵🪦")
+                                print("Your officer has dealt damage on the enemy 💥😱")
+                                BotSols[BotSols.index(currentDefender)] -= currentAttacker + experienceExtra
+                        else:
+                            injuries = randint(0, 1)
+                            if injuries == 0:
+                                PlayerOfficer.pop()
+                                BotOfficer[0] -= currentAttacker
+                                print("Your officer had been defeated! 😵🪦")
+                                print("Your officer has dealt damage on the enemy 💥😱")
+                            else:
+                                PlayerOfficer.pop()
+                                if len(PlayerSols) >= 2:
+                                    print("Your officer had been defeated! 😵🪦")
+                                    print("Your officer has dealt damage on the enemy 💥😱")
+                                    BotOfficer[0] -= currentAttacker
+                                else:
+                                    PlayerSols.append(1)
+                                    print("Your officer is demoted to a 1 rank soldier! 💂")
+                else:
+                    print("That soldier or officer does not exist!")
+                    print("Penalty: Lose a turn")
+                    warnings += 1
+    
+    elif cmd == "Swap Soldier":
+        if len(PlayerSols) == 0:
+            print("You don't have any soldiers!")
+            print("Penalty: Lose a turn")
+            warnings += 1
+        else:
+            cmd = int(input("Choose your desired soldier to swap ↩️: "))
+            retiring = cmd
+            if cmd in PlayerSols:
+                cmd = int(input("Choose your desired soldier to swap with ↪️: "))
+                advancing = cmd
+                if cmd in PlayerDeck:
+                    PlayerDeck.remove(advancing)
+                    PlayerDeck.append(retiring)
+                    PlayerSols.append(advancing)
+                    PlayerSols.remove(retiring)
+                    print("Soldier swapped! 🤸🏃")
+                else:
+                    print("That soldier does not exist!")
+                    print("Penalty: Lose a turn")
+                    warnings += 1
+            else:
+                print("That soldier does not exist!")
+                print("Penalty: Lose a turn")
+                warnings += 1
+    
+    lenP = len(PlayerDeck)
+    lenB = len(BotDeck)
+
+if lenP == 0:
+    print("You lost the war, better luck next time. 😭💀")
+else:
+    print("You're the champion! 😄🏆")
